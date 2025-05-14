98186cfeda8e8fd7a5ddc44675c427367eb07c52
YLZ515 <1922690847@qq.com>
2025-05-14 10:50:34 +0800

    Add files via upload

---
+++ page.js
@@ -0,0 +1,9 @@
+import Navbar from "./components/Navbar";
+export default function Hello() {
+  return (
+    <div>
+      <Navbar />
+        <h1 className="text-red-500 bg-sky-50 text-6xl text-center">Hello</h1>
+    </div>
+  )
+}
